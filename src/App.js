04d11967201c78c4b1b4205f8238f9a56cb433b8
Ross Buchan <rossbuchan1@googlemail.com>
2023-08-18 14:28:40 +0100

    Create add new todo function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ function App() {
     {name: "Water Plants", priority: "high"}
   ])
 
+  const [newToDo, setNewToDo] = useState("")
+
+  const handleToDoInput = (event) => {
+    setNewToDo(event.target.value)
+  }
+
+  const saveNewToDo = (event) => {
+    event.preventDefault()
+    const copyTodos= [...toDos]
+    copyTodos.push({name: newToDo, priority: "low"})
+    setToDos(copyTodos)
+  }
+
   const toDoNodes = toDos.map((toDo, index) => {
     return (
       <li key={index}>
@@ -21,6 +34,12 @@ function App() {
     <div className='App'>
       <h1>ToDo List</h1>
       <hr></hr>
+
+      <form onSubmit={saveNewToDo}>
+        <label htmlFor="new-toDo">Add a new toDo</label>
+        <input id="new-toDo" type="text" value={newToDo} onChange={handleToDoInput}></input>
+        <input id="new-toDo--submit" type="submit" value="Save new To-Do"></input>
+      </form>
 
       <ul>
         {toDoNodes}
